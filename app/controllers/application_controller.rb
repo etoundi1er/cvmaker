4e0c3929bc2618b912f9073e48413544bae20616
Frank Etoundi <frank.etoundi@gmail.com>
2018-05-05 17:34:26 +0200

    improve marketing design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :set_marketing_scope
 
     protected
 
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
         devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
+
+    def set_marketing_scope
+        @marketing_scope = true unless current_user
     end
 end
