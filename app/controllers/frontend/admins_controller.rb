6e88a974b776a1a80c064de8acc7efb80d8af514
Frank Etoundi <frank.etoundi@gmail.com>
2018-05-19 05:49:05 +0200

    check admin

--- app/controllers/frontend/admins_controller.rb
+++ app/controllers/frontend/admins_controller.rb
@@ -1,5 +1,6 @@
 module Frontend
     class AdminsController < FrontendController
+        before_action :check_admin
         before_action :set_user, only: %i[show_user destroy_user]
 
         def index
@@ -17,6 +18,10 @@ module Frontend
         end
 
         private
+
+        def check_admin
+            redirect_to root_path, alert: 'Hold on right there champion. You cannot access this page' unless current_user.admin?
+        end
 
         def set_user
             @user = User.find(params[:user_id])
